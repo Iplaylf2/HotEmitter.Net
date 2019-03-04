14235cd82059c4f26526b08b1258676d4ccc8506
IplayLF2 <914637118@qq.com>
2019-03-04 15:53:59 +0800

    添加项目文件。

---
+++ HotEmitter/Emitter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HotEmitter
+{
+    public class Emitter<T>
+    {
+        public void Emit<T>()
+        {
+        }
+
+        public Line<T> Line { get; }
+    }
+}
---
+++ HotEmitter/Line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotEmitter
+{
+    public class Line<T>
+    {
+        public Line(Emitter<T> emitter)
+        {
+        }
+
+        public Action Connect(Action<T> action)
+        {
+        }
+    }
+}
